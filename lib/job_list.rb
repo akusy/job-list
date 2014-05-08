b1c28f4a5d1a950461ab7d11473fff8c7e22fb74
Arkadiusz Kusy <arkadiusz.kusy@ironin.pl>
2014-05-08 20:39:28 +0200

    Add parse_jobs method Add parsen_job spec

--- lib/job_list.rb
+++ lib/job_list.rb
@@ -1,6 +1,15 @@
 require 'pry'
 
 class JobList
-  def initialize
+  def initialize list_of_jobs = ''
+    self.list_of_jobs = list_of_jobs
   end
+
+  def parse_jobs
+    return "" if @list_of_jobs.empty?
+  end
+
+  private
+  attr_accessor :list_of_jobs, :final_sequence
+
 end
--- spec/job_list_spec.rb
+++ spec/job_list_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe JobList do
+  describe "#parse_jobs" do
+
+    context "When empty string is given" do
+      subject { described_class.new "" }
+
+      it "returns empty sequence" do
+        expect(subject.parse_jobs).to be_empty
+      end
+    end
+  end
 end
